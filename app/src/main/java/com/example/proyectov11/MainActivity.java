3bcbb3e9481418097b7016ed0e1bbc00e95d6501
General <cristian.pasten.lda2018@gmail.com>
2024-10-01 15:22:33 -0300

    carga inicial del proyecto android v.1.9

--- app/src/main/java/com/example/proyectov11/MainActivity.java
+++ app/src/main/java/com/example/proyectov11/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.proyectov11;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
     }
+
+    public void goToLogin(View view) {
+        Intent intent = new Intent(MainActivity.this, Login.class);
+        startActivity(intent);
+    }
+
+    public void goToRegistro(View view) {
+        Intent intent = new Intent(MainActivity.this, Registro.class);
+        startActivity(intent);
+    }
+
+    public void goToPantalla(View view){
+        Intent intent = new Intent(MainActivity.this, PantallaPrincipal.class);
+        startActivity(intent);
+    }
+
+
+
+
 }
+
--- app/src/main/java/com/example/proyectov11/PantallaPrincipal.java
+++ app/src/main/java/com/example/proyectov11/PantallaPrincipal.java
@@ -1,6 +1,8 @@
 package com.example.proyectov11;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +15,12 @@ public class PantallaPrincipal extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_pantalla_principal);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.activity_pantalla);
+
+    }
+
+    public void goToCreate(View view){
+        Intent intent = new Intent(PantallaPrincipal.this, AgregarHabitos.class);
+        startActivity(intent);
     }
 }
