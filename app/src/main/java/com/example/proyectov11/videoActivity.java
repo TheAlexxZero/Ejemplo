a5d9ec1b034567ea95bd7b09a92a096bed0c68d1
General <cristian.pasten.lda2018@gmail.com>
2024-11-04 17:42:25 -0300

    "agregar multimedia"

---
+++ app/src/main/java/com/example/proyectov11/videoActivity.java
@@ -0,0 +1,10 @@
+package com.example.proyectov11;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.VideoView;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class videoActivity extends AppCompatActivity {
+}
